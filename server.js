ef4e952df7d98d8ccb2f6c2c0395eba49d35d855
Eric Moon <moonkangjin9195@gmail.com>
2022-09-14 11:51:03 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+const rappers = {
+    '21 savage':{
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England',
+    },
+    'chance the rapper':{
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois',
+    },
+    'dylan':{
+        'age': 29,
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan',
+    },
+}
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (req, res)=>{
+    const rappersName = req.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        res.json(rappers[rappersName])
+    }else{
+        res.json(rappers['dylan'])
+    }
+    // res.json(rappers)
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is running on port ${PORT}! You better go catch it!`)
+})
